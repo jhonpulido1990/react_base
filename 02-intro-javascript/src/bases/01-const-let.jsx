1381e3baeb80971503aa444795fe819b42bfa477
jhonpulido1990 <3825@holbertonstudents.com>
2025-04-27 20:42:14 -0500

    template string

---
+++ 02-intro-javascript/src/bases/01-const-let.jsx
@@ -0,0 +1,16 @@
+
+const nombre = 'jhon';
+const apellido = 'pulido';
+
+let valordado = 5;
+valordado = 4;
+
+console.log( nombre, apellido, valordado);
+let valor = true;
+// var no se debe usar
+if( valor ) {
+  const nombre = 'peter';
+  console.log(nombre);
+}
+
+console.log(valordado);
