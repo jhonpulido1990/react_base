86e353c65944dd5e2b601d861b82fa992b935d67
jhonpulido1990 <3825@holbertonstudents.com>
2025-04-27 21:00:02 -0500

    objeto literal

--- 02-intro-javascript/src/main.jsx
+++ 02-intro-javascript/src/main.jsx
@@ -1,2 +1,21 @@
 
-console.log('hola mundo!!!');
+
+const persona = {
+  nombre: 'tony',
+  apellido: 'start',
+  edad: 45,
+  direccion: {
+    ciudad: 'New York',
+    zip: 533335515,
+    lat: 14.3232,
+    lng: 34.92333321
+  }
+};
+
+// console.log(persona);
+
+const persona2 = {...persona};
+persona2.nombre = 'Perter'
+
+console.log(persona);
+console.log(persona2)
